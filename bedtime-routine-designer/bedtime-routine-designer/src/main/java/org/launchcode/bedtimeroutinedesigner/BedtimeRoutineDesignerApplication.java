b4fda8375322c12582ffa1a8bef760e5d4233acf
Lynelle Camp <lcamp118@live.com>
2019-02-27 21:41:42 -0600

    project setup with spring boot, gradle, intellij, and added a controller that prints hello world

---
+++ bedtime-routine-designer/bedtime-routine-designer/src/main/java/org/launchcode/bedtimeroutinedesigner/BedtimeRoutineDesignerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.bedtimeroutinedesigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedtimeRoutineDesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedtimeRoutineDesignerApplication.class, args);
+	}
+
+}

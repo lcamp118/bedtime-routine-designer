df1b38c918a3beb05e9e777ddb6f8abb451b171d
Lynelle Camp <lcamp118@live.com>
2019-03-11 20:06:31 -0500

    added request handler for routine index page

--- bedtime-routine-designer/bedtime-routine-designer/src/main/java/com/lynelleherrera/bedtimeroutinedesigner/Controllers/RoutineController.java
+++ bedtime-routine-designer/bedtime-routine-designer/src/main/java/com/lynelleherrera/bedtimeroutinedesigner/Controllers/RoutineController.java
@@ -9,10 +9,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("routine")
 public class RoutineController {
 
+    @RequestMapping(value="", method=RequestMethod.GET)
+    public String displayRoutines(Model model){
+        model.addAttribute("title", "My Routines");
+        return "routine/index";
+    }
+
     @RequestMapping(value="add", method= RequestMethod.GET)
     public String displayAddRoutineForm(Model model){
         model.addAttribute("title", "Add Routine");
         return "routine/add";
     }
+
+    @RequestMapping(value="add", method=RequestMethod.POST)
+    public String processAddRoutineForm(Model model){
+        model.addAttribute("title", "Create Routine");
+        return "redirect:";
+    }
+
+    //#TODO add delete functionality
 
 }

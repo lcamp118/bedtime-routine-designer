cf589e113d44e0c6fdc5a9ad72fa50705300dfac
Lynelle Camp <lcamp118@live.com>
2019-03-11 20:43:12 -0500

    added link in fragments to add activity and created activity templates to add and display all activities and added request handlers in activity controller

--- bedtime-routine-designer/bedtime-routine-designer/src/main/java/com/lynelleherrera/bedtimeroutinedesigner/Controllers/ActivityController.java
+++ bedtime-routine-designer/bedtime-routine-designer/src/main/java/com/lynelleherrera/bedtimeroutinedesigner/Controllers/ActivityController.java
@@ -1,12 +1,30 @@
 package com.lynelleherrera.bedtimeroutinedesigner.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("activity")
 public class ActivityController {
 
-    //#TODO display all activities
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public String displayActivities(Model model){
+        model.addAttribute("title", "All Activities");
+        return "activity/index";
+    }
+
+    @RequestMapping(value="add", method=RequestMethod.GET)
+    public String addActivities(Model model){
+        model.addAttribute("title", "Add Activity");
+        return "activity/add";
+    }
+
+    @RequestMapping(value="add", method=RequestMethod.POST)
+    public String processAddActivitiesForm(Model model){
+        model.addAttribute("title", "Add Activity");
+        return "activity/index";
+    }
 
 }

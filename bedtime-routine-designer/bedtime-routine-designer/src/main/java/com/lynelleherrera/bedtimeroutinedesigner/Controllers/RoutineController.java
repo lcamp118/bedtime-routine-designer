4c4c290dbd029ec8d34a38071d58442781ae70dc
Lynelle Camp <lcamp118@live.com>
2019-03-11 19:21:38 -0500

    created add routine template and request handler to routine controller

--- bedtime-routine-designer/bedtime-routine-designer/src/main/java/com/lynelleherrera/bedtimeroutinedesigner/Controllers/RoutineController.java
+++ bedtime-routine-designer/bedtime-routine-designer/src/main/java/com/lynelleherrera/bedtimeroutinedesigner/Controllers/RoutineController.java
@@ -1,12 +1,18 @@
 package com.lynelleherrera.bedtimeroutinedesigner.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("routine")
 public class RoutineController {
 
-
+    @RequestMapping(value="add", method= RequestMethod.GET)
+    public String displayAddRoutineForm(Model model){
+        model.addAttribute("title", "Add Routine");
+        return "routine/add";
+    }
 
 }
